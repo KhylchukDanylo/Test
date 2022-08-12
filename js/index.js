046c716b9be2dd2dda377da4cfcd0f625c2c97cd
Danilo <hilcukdanilo@gmail.com>
2022-08-12 15:34:45 +0200

    dd

--- js/index.js
+++ js/index.js
@@ -42,3 +42,18 @@
 // delArr(2, 8);
 // delArr(10, 1);
 // delArr(-9, 24);
+
+function checkStorage(available, ordered) {
+  if (ordered === 0) {
+    return 'Your order is empty!';
+  } else if (ordered > available) {
+    return 'Your order is too large, not enough goods in stock!';
+  }
+  return 'The order is accepted, our manager will contact you';
+}
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(150, 0));
