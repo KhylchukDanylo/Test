be0fc634dd7eb07ce7d14bb88258f2b1d6fea8ef
Danilo <hilcukdanilo@gmail.com>
2022-08-31 16:42:58 +0200

    ff

--- js/index.js
+++ js/index.js
@@ -521,3 +521,26 @@
 // const alphabeticalAuthors = [...authors].sort();
 // console.log(ascendingReleaseDates, `-----`, releaseDates, `===`, ascendingReleaseDates === releaseDates);
 // console.log(alphabeticalAuthors, `-----`, authors, `===`, alphabeticalAuthors === authors);
+
+class Storage {
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    // this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.items.includes(itemToRemove)) {
+      const indexRemoveElem = this.indecOf(itemToRemove);
+      this.splice(indexRemoveElem, 1);
+    }
+  }
+}
+
+// Change code above this line
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
