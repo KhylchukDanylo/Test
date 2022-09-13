dad36e48f138e783c57a5573b7a32fd72e8df13c
Danilo <hilcukdanilo@gmail.com>
2022-09-13 12:43:30 +0200

    f

---
+++ js/testDom.js
@@ -0,0 +1,13 @@
+const image = document.querySelector.textContent('.text__video');
+image.textContent = 'Hello bro';
+console.log(image.textContent);
+
+// const textColor = document.querySelector('.text__video');
+// textColor.style.color = 'teal';
+// console.log(textColor.style.color);
+
+// const text = document.createElement('h1');
+// text.textContent = 'Lorem hello world!';
+// text.after(text);
+// text.style.zIndex = '2';
+// console.log(text.textContent);
