0dcb72e7f952863bbf6f7ba265e72fb034214d1e
Danilo <hilcukdanilo@gmail.com>
2022-11-07 18:40:17 +0100

    addcfile

---
+++ src/js/auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWFLAsZ9YlrUnKpHpWHRY4IJD4zpUKS5I',
+  authDomain: 'photo-fcc53.firebaseapp.com',
+  projectId: 'photo-fcc53',
+  storageBucket: 'photo-fcc53.appspot.com',
+  messagingSenderId: '476532335187',
+  appId: '1:476532335187:web:35022358bd031374b3dca6',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
